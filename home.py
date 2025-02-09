76d7d90d4de46f5c8456dde2f902a4934a7d972e
gretmova <110213988+gretmova@users.noreply.github.com>
2025-02-09 17:23:03 -0600

    Add files via upload

---
+++ home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Welcome to my first chatbot")
+st.divider()
+st.write("Interact with different versions of the chatbot from the sidebar")
+
+st.image("https://easy-peasy.ai/ai-image-generator/images/cat-coding-on-laptop-ingenious-feline-tech-prowess")
+
+st.subheader("Built with love @gretmova 😊💕")
